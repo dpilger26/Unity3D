da6d1ad6e471938fed1980505338c75c81a0cb6e
David Pilger <dpilger26@gmail.com>
2019-03-11 23:10:39 -0600

    Lecture 46

---
+++ ProjectBoost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // configuration parameters
+    [SerializeField] float thrust = 5f;
+    [SerializeField] float rcsThrust = 5f;
+
+    // cached references
+    Rigidbody myRigidBody;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRigidBody.AddRelativeForce(Vector3.up * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rcsThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rcsThrust);
+        }
+    }
+}
