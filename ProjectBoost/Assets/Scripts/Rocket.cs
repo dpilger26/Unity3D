310c873d4239f3c08640a9cef5f3bbcb1cedf0e4
David Pilger <dpilger26@gmail.com>
2019-03-12 22:35:30 -0600

    Lecture 47

--- ProjectBoost/Assets/Scripts/Rocket.cs
+++ ProjectBoost/Assets/Scripts/Rocket.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     // configuration parameters
+    [Header("Thrust Parameters")]
     [SerializeField] float thrust = 5f;
     [SerializeField] float rcsThrust = 5f;
 
     // cached references
     Rigidbody myRigidBody;
+    AudioSource myAudioSource;
 
     // Start is called before the first frame update
     private void Start()
     {
         myRigidBody = GetComponent<Rigidbody>();
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         ProcessInput();
+        ToggleThrustSFX(Input.GetKey(KeyCode.Space));
     }
 
     private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            ToggleThrustSFX(true);
             myRigidBody.AddRelativeForce(Vector3.up * thrust);
         }
 
@@ -38,5 +43,10 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(Vector3.back * rcsThrust);
         }
+    }
+
+    private void ToggleThrustSFX(bool flag)
+    {
+        myAudioSource.enabled = flag;
     }
 }
