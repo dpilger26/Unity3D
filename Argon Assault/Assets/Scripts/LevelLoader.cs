db8ac932087ca8905a479da284f49c05832f325c
David Pilger <dpilger26@gmail.com>
2019-03-19 22:15:07 -0600

    Lecture 81

---
+++ Argon Assault/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // configuration parameters
+    [SerializeField] float splashScreenWaitSeconds = 3f;
+
+    private void Start()
+    {
+        StartCoroutine(LoadStartMenu());
+    }
+
+    private IEnumerator LoadStartMenu()
+    {
+        yield return new WaitForSecondsRealtime(splashScreenWaitSeconds);
+        SceneManager.LoadScene(1);
+    }
+}
