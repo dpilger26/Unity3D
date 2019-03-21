562a3e33a11e791cdaf355c62603e73a9d4b9451
David Pilger <dpilger26@gmail.com>
2019-03-20 22:51:17 -0600

    Lecture 86

--- Argon Assault/Assets/Scripts/Player.cs
+++ Argon Assault/Assets/Scripts/Player.cs
@@ -6,19 +6,22 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     // configuration parameters
-    [Tooltip("In m/s")]
-    [SerializeField] float xSpeed = 4f;
-    [Tooltip("In m/s")]
-    [SerializeField] float ySpeed = 4f;
+    [Header("Movement Parameters")]
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-
-    }
+    [SerializeField] [Tooltip("Local coordinates")] float minXOffset = -5f;
+    [SerializeField] [Tooltip("Local coordinates")] float maxXOffset = 5f;
+    [SerializeField] [Tooltip("Local coordinates")] float minYOffset = -5f;
+    [SerializeField] [Tooltip("Local coordinates")] float maxYOffset = 5f;
+    [SerializeField] [Tooltip("In m/s")] float xSpeed = 10f;
+    [SerializeField] [Tooltip("In m/s")] float ySpeed = 10f;
 
     // Update is called once per frame
     private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
@@ -26,6 +29,9 @@ public class Player : MonoBehaviour
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float yOffset = yThrow * ySpeed * Time.deltaTime;
 
-        Debug.Log(xOffset);
+        float newXPos = Mathf.Clamp(transform.localPosition.x + xOffset, minXOffset, maxXOffset);
+        float newYPos = Mathf.Clamp(transform.localPosition.y + yOffset, minYOffset, maxYOffset);
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
     }
 }
