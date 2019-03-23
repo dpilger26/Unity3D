9779050f8d95ea988cfc47060782c40ec4a13a50
David Pilger <dpilger26@gmail.com>
2019-03-23 12:11:16 -0600

    Lecture 96

---
+++ Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with " + other.name);
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        Debug.Log("Player Dying");
+        SendMessage("OnPlayerDeath");
+    }
+}
--- Argon Assault/Assets/Scripts/PlayerController.cs
+++ Argon Assault/Assets/Scripts/PlayerController.cs
@@ -4,35 +4,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // configuration parameters
-    [Header("Movement Parameters")]
+    [Header("Limit Parameters")]
     [SerializeField] [Tooltip("Local coordinates")] float minXOffset = -8f;
     [SerializeField] [Tooltip("Local coordinates")] float maxXOffset = 8f;
     [SerializeField] [Tooltip("Local coordinates")] float minYOffset = -5f;
     [SerializeField] [Tooltip("Local coordinates")] float maxYOffset = 5f;
+
+    [Header("Position Parameters")]
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = 5.5f;
+
+    [Header("Control Parameters")]
     [SerializeField] [Tooltip("In m/s")] float xSpeed = 12f;
     [SerializeField] [Tooltip("In m/s")] float ySpeed = 12f;
-    [SerializeField] float positionPitchFactor = -5f;
     [SerializeField] float controlPitchFactor = -20f;
-    [SerializeField] float positionYawFactor = 5.5f;
     [SerializeField] float controlRollFactor = -30f;
 
     // state parameters
-    float xThrow;
-    float yThrow;
+    float xThrow = 0f;
+    float yThrow = 0f;
+    bool isControlEnabled = true;
 
     // Update is called once per frame
     private void Update()
     {
-        Move();
-        Rotate();
+        if (isControlEnabled)
+        {
+            Move();
+            Rotate();
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnPlayerDeath() // Called by string reference elsewhere
     {
-        Debug.Log("Collided with " + other.name);
+        Debug.Log("Disabling player controls.");
+        isControlEnabled = false;
     }
 
     private void Move()
