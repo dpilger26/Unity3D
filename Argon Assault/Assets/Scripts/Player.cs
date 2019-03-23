505a9ad5be2089f1bd3d8e1591f4f91e8bb3e571
David Pilger <dpilger26@gmail.com>
2019-03-22 22:30:30 -0600

    Lecture 89

--- Argon Assault/Assets/Scripts/Player.cs
+++ Argon Assault/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -7,24 +8,32 @@ public class Player : MonoBehaviour
 {
     // configuration parameters
     [Header("Movement Parameters")]
-
     [SerializeField] [Tooltip("Local coordinates")] float minXOffset = -5f;
     [SerializeField] [Tooltip("Local coordinates")] float maxXOffset = 5f;
     [SerializeField] [Tooltip("Local coordinates")] float minYOffset = -5f;
     [SerializeField] [Tooltip("Local coordinates")] float maxYOffset = 5f;
     [SerializeField] [Tooltip("In m/s")] float xSpeed = 20f;
     [SerializeField] [Tooltip("In m/s")] float ySpeed = 20f;
+    [SerializeField] float positionPitchFactor = -3f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 4f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    // state parameters
+    float xThrow;
+    float yThrow;
 
     // Update is called once per frame
     private void Update()
     {
         Move();
+        Rotate();
     }
 
     private void Move()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float yOffset = yThrow * ySpeed * Time.deltaTime;
@@ -33,5 +42,17 @@ public class Player : MonoBehaviour
         float newYPos = Mathf.Clamp(transform.localPosition.y + yOffset, minYOffset, maxYOffset);
 
         transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+    }
+
+    private void Rotate()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControl = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControl;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
