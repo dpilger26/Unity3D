1b1a32ab85a0450bdad22206da3d9cdeaefc98a5
David Pilger <dpilger26@gmail.com>
2019-03-24 12:52:03 -0600

    Lecture 104

---
+++ Argon Assault/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+    // cached parameters
+    TextMeshProUGUI scoreText;
+
+    // state parameters
+    int score = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void AddToScore(int amount)
+    {
+        score += amount;
+        UpdateScore();
+    }
+}
