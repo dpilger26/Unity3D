bc92ea76dace78019082e5e85c9d9661f8164728
David Pilger <dpilger26@gmail.com>
2019-04-01 23:00:17 -0600

    Lecture 124

--- RealmRush/Assets/Scripts/CubeEditor.cs
+++ RealmRush/Assets/Scripts/CubeEditor.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode] // makes the script run in the editor mode
-public class EditorSnap : MonoBehaviour
+[SelectionBase] // makes it easier to select to object in the editor
+public class CubeEditor : MonoBehaviour
 {
+    [Header("Grid Snapping Parameters")]
     [SerializeField] [Range(1f, 20f)] float xGridSize = 10f;
     [SerializeField] [Range(1f, 20f)] float yGridSize = 10f;
     [SerializeField] [Range(1f, 20f)] float zGridSize = 10f;
 
     void Update()
+    {
+        SnapPosition();
+        UpdateLabel();
+    }
+
+    private void SnapPosition()
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / xGridSize) * xGridSize;
@@ -17,5 +25,15 @@ public class EditorSnap : MonoBehaviour
         snapPos.z = Mathf.RoundToInt(transform.position.z / xGridSize) * zGridSize;
 
         transform.position = snapPos;
+    }
+
+    private void UpdateLabel()
+    {
+        var labelTextMesh = GetComponentInChildren<TextMesh>();
+
+        var xPosStr = (transform.position.x / xGridSize).ToString();
+        var zPosStr = (transform.position.z / yGridSize).ToString();
+
+        labelTextMesh.text = xPosStr + "," + zPosStr;
     }
 }
