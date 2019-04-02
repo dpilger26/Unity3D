a4c26f6b19967f6892eef41c19c38fe95f5ff5e2
David Pilger <dpilger26@gmail.com>
2019-04-01 22:30:47 -0600

    Lecture 123

---
+++ RealmRush/Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode] // makes the script run in the editor mode
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float xGridSize = 10f;
+    [SerializeField] [Range(1f, 20f)] float yGridSize = 10f;
+    [SerializeField] [Range(1f, 20f)] float zGridSize = 10f;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / xGridSize) * xGridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / xGridSize) * yGridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / xGridSize) * zGridSize;
+
+        transform.position = snapPos;
+    }
+}
