c8286d3579f572cf74fe726479938490ef3e444a
David Pilger <dpilger26@gmail.com>
2019-04-02 22:57:22 -0600

    Lecture 126

--- RealmRush/Assets/Scripts/EnemyMovement.cs
+++ RealmRush/Assets/Scripts/EnemyMovement.cs
@@ -5,25 +5,23 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     // configuration parameters
-    [SerializeField] List<Block> path;
+    [SerializeField] List<Waypoint> path;
 
     // Start is called before the first frame update
     void Start()
     {
-        PrintAllWaypoints();
+        StartCoroutine(FollowPath());
     }
 
-    private void PrintAllWaypoints()
+    private IEnumerator FollowPath()
     {
-        foreach (var block in path)
+        Debug.Log("Starting Patrol");
+        foreach (var waypoint in path)
         {
-            Debug.Log(block.name);
+            Debug.Log("Visiting " + waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSecondsRealtime(1f);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Ending Patrol");
     }
 }
