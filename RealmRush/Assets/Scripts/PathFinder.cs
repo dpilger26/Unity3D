734e9b6d64fd6d68ff65dedfb4978479db959f2c
David Pilger <dpilger26@gmail.com>
2019-04-03 22:42:48 -0600

    Lecture 130

---
+++ RealmRush/Assets/Scripts/PathFinder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    Dictionary<Vector3Int, Waypoint> grid = new Dictionary<Vector3Int, Waypoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach(var waypoint in waypoints)
+        {
+            var waypointGridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(waypointGridPos))
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint.name);
+            }
+            else
+            {
+                grid.Add(waypointGridPos, waypoint);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
